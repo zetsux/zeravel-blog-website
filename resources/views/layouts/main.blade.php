028f6eb7aec52d67251eecf013d05a47d9072f95
zetsuen <kevnhalim@gmail.com>
2023-02-01 22:39:20 +0700

    feat : changing color using color palette reference

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
     <title>Zeravel Blog | {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
-  <body>
+  <body style="background-color:#F6F6F6">
     
     @include('partials.navbar')
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg" style="background-color:midnightblue;" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg" style="background-color:#0078AA;" data-bs-theme="dark">
     <div class="container-fluid ms-4">
       <a class="navbar-brand" href="/">Zeravel Blog</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
