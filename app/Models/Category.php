4a6a05512c4ca98eb66e5c65533adb3955687d9c
zetsuen <kevnhalim@gmail.com>
2023-02-02 16:12:42 +0700

    feat : moving lazy eager loading of category to controller

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,7 @@ class Category extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'slug'];
+    protected $load = ['user', 'category'];
 
     public function posts() {
         return $this->hasMany(Post::class);
