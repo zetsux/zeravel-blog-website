d982d42dcdfebf898cc540dd3f2988c45109609a
zetsuen <kevnhalim@gmail.com>
2023-02-02 23:16:57 +0700

    fix : fixing title when searching by category or user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,12 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function showPosts() {
+        $title = '';
+
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = $category->name;
+        }
+
+        if (request('user')) {
+            $user = User::firstWhere('username', request('user'));
+            $title = $user->name . "'s";
+        }
+
         return view('blog', [
-            "title" => "All",
+            "title" => $title,
             "posts" => Post::latest()->filter(request(['search', 'category', 'user']))->get(),
             'type' => "All"
         ]);
