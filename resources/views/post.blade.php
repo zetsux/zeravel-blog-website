3a86dd9cc35880bd3b9b3a5a7b7e6d47c545d1bd
zetsuen <kevnhalim@gmail.com>
2023-02-02 17:12:20 +0700

    feat : changing UI of post view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,24 @@
 @extends("layouts.main")
 
 @section("container")
-    <h2>{{ $post->title }}</h2>
-    <h5>By : <a href="/author/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+    
+    <div class="container-fluid ms-1">
+        <div class="row justify-content-center mb-4">
+            <div class="col-md-8">
+                <h2 class="mb-2">{{ $post->title }}</h2>
+                <h6 class="mb-4">By : <a href="/author/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a> ({{ $post->created_at->diffForHumans() }})</h6>
 
-    @if (str_contains($post->content, '</p>')) {!! $post->content !!}
-    @else {{ $post->content }}<br>
-    @endif
+                <img src="https://source.unsplash.com/1200x300?{{ $post->category->name }}" alt="Post Image" class="img-fluid">
 
-    <br>
-    <a href="/blog">Go back</a>
+                <article class="my-4">
+                    @if (str_contains($post->content, '</p>')) {!! $post->content !!}
+                    @else {{ $post->content }}<br>
+                    @endif
+                </article>
+
+                <a href="/blog" class="text-decoration-none text-light btn btn-primary">⬅️ Back</a>
+            </div>
+        </div>
+    </div>
+
 @endsection
