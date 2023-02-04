dde8e487813658afd3e4e68e086046719bfd45a2
zetsuen <kevnhalim@gmail.com>
2023-02-04 18:37:19 +0700

    fix : fixing active according to the page

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -2,13 +2,13 @@
     <div class="position-sticky pt-3 sidebar-sticky">
       <ul class="nav flex-column">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/dashboard">
+          <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="/dashboard">
             <span data-feather="home" class="align-text-bottom"></span>
             Dashboard
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/dashboard/posts">
+          <a class="nav-link {{ Request::is('dashboard/posts') ? 'active' : '' }}" href="/dashboard/posts">
             <span data-feather="file-text" class="align-text-bottom"></span>
             Your Posts
           </a>
