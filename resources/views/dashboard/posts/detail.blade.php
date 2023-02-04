82794a496a2074a46c53cfadfa96433ff10b71b2
zetsuen <kevnhalim@gmail.com>
2023-02-04 21:19:33 +0700

    feat : adjusting alignment of icons

--- resources/views/dashboard/posts/detail.blade.php
+++ resources/views/dashboard/posts/detail.blade.php
@@ -5,10 +5,10 @@
         <div class="row my-4">
             <div class="col-lg-12">
                 <div class="d-flex justify-content-between mb-4">
-                    <a href="/dashboard/posts" class="text-decoration-none text-light btn btn-info"><span data-feather="arrow-left-circle"></span> Back to Posts</a>
+                    <a href="/dashboard/posts" class="text-decoration-none text-light btn btn-info"><span data-feather="arrow-left-circle" style="margin-bottom:3px"></span> Back to Posts</a>
                     <div>
-                        <a href="#" class="text-decoration-none text-light btn btn-warning"><span data-feather="edit-3"></span> Edit</a>
-                        <a href="#" class="text-decoration-none text-light btn btn-danger"><span data-feather="trash-2"></span> Delete</a>
+                        <a href="#" class="text-decoration-none text-light btn btn-warning"><span data-feather="edit-3" style="margin-bottom:3px"></span> Edit</a>
+                        <a href="#" class="text-decoration-none text-light btn btn-danger"><span data-feather="trash-2" style="margin-bottom:3px"></span> Delete</a>
                     </div>
                 </div>
 
