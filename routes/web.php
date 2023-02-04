0ea633376ab254b5a601220d9a318c0bbe1cb775
zetsuen <kevnhalim@gmail.com>
2023-02-04 16:42:16 +0700

    refactor : deleting DashboardController and using a closure function instead

--- routes/web.php
+++ routes/web.php
@@ -51,4 +51,6 @@ Route::post('/logout', [LoginController::class, 'out']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
